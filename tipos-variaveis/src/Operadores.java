dd352504379c0fbc23752b87ec67ec27d754334b
Fabio-PN <fabpnog@gmail.com>
2023-02-25 16:36:53 -0300

    Incremento, decremento e inversão de valores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,5 +1,6 @@
 
 public class Operadores {
+    // Concatenação
     public static void main(String[] args) {
         String nomeCompleto = "LINGUAGEM" + "JAVA";
         System.out.println(nomeCompleto);
@@ -20,5 +21,29 @@ public class Operadores {
 
         concatenacao = (1+1+1+1)+"1";
         System.out.println(concatenacao);
+
+        int numero = 5;
+        System.out.println(-numero);
+
+
+        // Incrementos e decrementos de valores
+        int valor = 5;
+        //x repetição
+        System.out.println(valor);
+        valor = valor + 2;
+        System.out.println(valor);
+        System.out.println(++valor);
+        System.out.println(valor);
+        System.out.println(valor--);
+        System.out.println(valor);
+
+        // Validando valores de uma variável como true or false
+        boolean variavel = true;
+        variavel = !variavel;
+        System.out.println(variavel);
+        variavel = !variavel;
+        System.out.println(variavel);
+    
     }
+    
 }
